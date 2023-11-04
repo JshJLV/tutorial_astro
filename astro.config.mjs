8a1e411a1067467a8e4a563ef8ef9ced59e8ae69
Joseph Lopez <joseph.lpz1999@gmail.com>
2023-11-04 17:53:29 -0600

    add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 
+  site: "https://astro-jsh.netlify.app",
 });
